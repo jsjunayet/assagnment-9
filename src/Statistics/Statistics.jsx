a4514039a8fd597e245fd0de16a6a00735361c4e
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2023-09-26 11:08:23 +0600

    added piechart in statistics

--- src/Statistics/Statistics.jsx
+++ src/Statistics/Statistics.jsx
@@ -1,53 +1,49 @@
-import React, { PureComponent } from 'react';
-import { render } from 'react-dom';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Sector, Cell, } from 'recharts';
 
 const Statistics = () => {
-    const data = [
-        { name: 'Group A', value: 400 },
-        { name: 'Group B', value: 300 },
-        { name: 'Group C', value: 300 },
-        { name: 'Group D', value: 200 },
+
+
+    const data01 = [
+        { name: 'Group A', value: 12 },
+        { name: 'Group B', value: 4 },
+
     ];
-    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-    const RADIAN = Math.PI / 180;
-    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-        const x = cx + radius * Math.cos(-midAngle * RADIAN);
-        const y = cy + radius * Math.sin(-midAngle * RADIAN);
-        return (
-            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-                {`${(percent * 100).toFixed(0)}%`}
-            </text>
-        );
-    };
-
-
-    render()
-    {
-        return (
-            <ResponsiveContainer width="100%" height="100%">
-                <PieChart width={400} height={400}>
+    const COLORS = ['#FF444A', '#0088FE'];
+    return (
+        <div className='flex justify-center items-center'>
+            <div className=''>
+                <PieChart width={500} height={400}>
                     <Pie
-                        data={data}
+                        data={data01}
                         cx="50%"
-                        cy="50%"
-                        labelLine={false}
-                        label={renderCustomizedLabel}
+                        cy="60%"
+                        labelLine={true}
+                        label
                         outerRadius={80}
                         fill="#8884d8"
                         dataKey="value"
                     >
-                        {data.map((entry, index) => (
+                        {data01.map((entry, index) => (
                             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                         ))}
                     </Pie>
                 </PieChart>
-            </ResponsiveContainer>
-        );
-    }
-}
+                <div className='flex gap-5'>
+                    <div className='flex gap-2 items-center'>
+                        <p className='text-xl text-[#0B0B0B]'>Your Donation</p>
+                        <div className='h-2 w-20 bg-[#00C49F] mt-1 rounded-sm'>
+                        </div>
+                    </div>
+                    <div className='flex gap-2 items-center '>
+                        <p className='text-xl text-[#0B0B0B]'>Total Donation</p>
+                        <div className='h-2 w-20 bg-[#FF444A] mt-1 rounded-sm'>
+                        </div>
+                    </div>
+                </div>
 
-
+            </div>
+        </div>
+    );
+};
 
 export default Statistics;
